94b7c78ecd0cb0907509c9c00ca919226a2b302a
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-11 18:11:08 +0100

    initial bootstrap

--- apps/ringface-fe/src/app/app.module.ts
+++ apps/ringface-fe/src/app/app.module.ts
@@ -3,9 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { AccordionModule } from 'ngx-bootstrap/accordion'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DaysEventsComponent } from './days-events/days-events.component';
+
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, DaysEventsComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AlertModule.forRoot(),
+    AccordionModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/ringface-fe/src/app/days-events/days-events.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ringface-days-events',
+  templateUrl: './days-events.component.html',
+  styleUrls: ['./days-events.component.scss']
+})
+export class DaysEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+const EVENTS =  [{
+  "id": 6937995083683256159,
+  "createdAt": "20210310-120421",
+  "answered": false,
+  "kind": "ding",
+  "duration": 31.0
+},
+{
+  "id": 6938074484743657311,
+  "createdAt": "20210310-171228",
+  "answered": true,
+  "kind": "ding",
+  "duration": 19.0
+}]
+
+interface ProcessedEvent{
+
+}
