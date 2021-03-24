d12e9b32135f76045e83cf348e041a8e0ffb6c85
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-24 12:00:11 +0100

    basic nav

---
+++ apps/ringface-fe/src/app/persons/persons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ringface-persons',
+  templateUrl: './persons.component.html',
+  styleUrls: ['./persons.component.scss']
+})
+export class PersonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
