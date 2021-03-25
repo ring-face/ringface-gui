126f620046896d75f0edc14d0ee2a48ae6b2f22f
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-25 09:09:24 +0100

    configurable docker builders

--- dockerImagesBuild.sh
+++ dockerImagesBuild.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
+
 docker build --tag ringface/bff:latest -f Dockerfile.bff .
 docker build --tag ringface/nginx:latest -f Dockerfile.nginx .
+
+if [ -z ${1+x} ]
+  then
+  echo "semver is not defined"
+  else
+  echo "tagging to '$1'"
+  docker tag ringface/bff:latest ringface/bff:$1
+  docker tag ringface/nginx:latest ringface/nginx:$1
+fi
--- dockerImagesPush.sh
+++ dockerImagesPush.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 
 docker push ringface/bff:latest
 docker push ringface/nginx:latest
+
+if [ -z ${1+x} ]
+  then
+  echo "semver is not defined"
+  else
+  echo "tagging to '$1'"
+  docker push ringface/bff:$1
+  docker push ringface/nginx:$1
+fi
+
+
