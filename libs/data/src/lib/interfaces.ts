6173b22978edabaa133d6380fbe6fa9408c2784a
Csaba Meszaros <abax.soraszem@gmail.com>
2021-03-25 19:23:49 +0100

    Add files via upload

---
+++ libs/data/src/lib/interfaces.ts
@@ -0,0 +1,62 @@
+export interface RingEvent {
+  eventName: string,
+  ringId: string,
+  id: number,
+  createdAt: Date,
+  date:string
+  answered: boolean,
+  kind: string,
+  duration: number,
+  status: "UNPROCESSED" | "PROCESSED",
+  videoFileName: string,
+  processingResult?:ProcessingResult,
+  processingTriggered?:boolean
+}
+
+export interface ProcessingResult{
+  videoFile: string,
+  eventName: string,
+  recognisedPersons: string[]
+  unknownPersons: UnknownPerson[]
+}
+
+export interface UnknownPerson{
+  buttonDisabled: boolean;
+  name:string,
+  images: number,
+  imagePaths: string[]
+}
+
+export interface DownloadFromRingResponse{
+  eventCount:number
+}
+
+export interface ProcessEventResponse{
+  videoFile:string
+  recognisedPersons: string [],
+  unknownPersons: string []
+}
+
+export interface FittingResult{
+  persons: PersonImages[],
+  fittedClassifierFile: string
+}
+
+export interface TagPersonRequest{
+  eventName: string,
+  unknownPerson: UnknownPerson,
+  newName:string
+}
+
+export interface TagPersonResponse{
+  message: string
+}
+
+export interface PersonImages{
+  personName:string,
+  imagePaths: string[]
+}
+
+export interface FitClassifierRequest{
+  persons: PersonImages[]
+}
