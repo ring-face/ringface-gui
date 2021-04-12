cd902982c4892c7e41bfea60c67b0e69e47c2a93
abaxsoraszem <abax.soraszem@gmail.com>
2021-04-12 12:28:28 +0200

    bounce the package.json version

--- dockerImagesBuild.sh
+++ dockerImagesBuild.sh
@@ -2,15 +2,16 @@
 
 cd "$(dirname "$0")"
 
-
-docker build --tag ringface/bff:latest -f Dockerfile.bff .
-docker build --tag ringface/nginx:latest -f Dockerfile.nginx .
-
 if [ -z ${1+x} ]
   then
-  echo "semver is not defined"
+    echo "semver is not defined"
   else
-  echo "tagging to '$1'"
-  docker tag ringface/bff:latest ringface/bff:$1
-  docker tag ringface/nginx:latest ringface/nginx:$1
+    npm version $1
+
+    docker build --tag ringface/bff:latest -f Dockerfile.bff .
+    docker build --tag ringface/nginx:latest -f Dockerfile.nginx .
+
+    echo "tagging to '$1'"
+    docker tag ringface/bff:latest ringface/bff:$1
+    docker tag ringface/nginx:latest ringface/nginx:$1
 fi
